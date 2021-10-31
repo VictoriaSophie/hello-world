ff58ff74f1bb2d00bf8c14e21579ae327a41b81d
Victoria <2004466@uad.ac.uk>
2021-10-31 15:49:33 +0000

    Put actual code in to print hello world

--- .vs/Source1.cpp
+++ .vs/Source1.cpp
@@ -1,1 +1,12 @@
-Console.WriteLine("Hello World!");
+using System;
+
+namespace helloWorld
+{
+	class Source1
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
